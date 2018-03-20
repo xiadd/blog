f23ae3a69410a914bb790e5c45720944665cfc6f
xiadd <xiadd0102@gmail.com>
2018-03-20 23:16:31 +0800

    change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-
-//components
-import Emoji from './Emoji';
 
 //styles
-import './App.scss';
-import './App.less';
-import './App.styl';
-
-//modules
-import cssStyles from './First.module.css';
-import sassStyles from './Second.module.scss';
-import lessStyles from './Third.module.less';
-import stylusStyles from './Fourth.module.styl';
+import './App.scss'
 
 class App extends Component {
   render() {
