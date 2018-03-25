0a9a290289972b26276320a98ee5b46bc6b86769
xiadd <xiadd0102@gmail.com>
2018-03-25 20:48:53 +0800

    change

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
-import { Card } from 'semantic-ui-react'
-import ReactMarkdown from 'react-markdown'
+import { Card, Loader } from 'semantic-ui-react'
+import marked from 'marked'
+import Prism from 'prismjs'
+import 'prismjs/components/prism-jsx';
+import 'prismjs/themes/prism.css';
 import { withRouter } from 'react-router-dom'
 import { getIssue } from 'services/issue'
+import './index.scss'
 
 @withRouter
 class Post extends Component {
   constructor () {
     super()
     this.state = {
-      issue: {}
+      issue: {},
+      loading: true
     }
   }
 
@@ -17,25 +22,33 @@ class Post extends Component {
     const { match } = this.props
     const id = match.params.id
     const issue = await getIssue(id)
+    document.title = issue.data.title
     this.setState({
-      issue: issue.data
+      issue: issue.data,
+      loading: false
     })
+  }
+  
+  componentDidUpdate () {
+    Prism.highlightAll()
   }
 
   render () {
     const { issue } = this.state
+    const html = issue.body && marked(issue.body)
     return (
       <Card fluid>
         <Card.Content>
+          <Loader active={this.state.loading} inline='centered' />
           <Card.Header>
             {issue.title}
           </Card.Header>
 
-          <Card.Description>
-            <ReactMarkdown 
-              source={issue.body} 
-            />
-          </Card.Description>
+          <Card.Meta>
+            {issue.html_url && <a href={issue.html_url}>查看原文</a>}
+          </Card.Meta>
+
+          <Card.Description className="markdown" dangerouslySetInnerHTML={{__html: html}} />
         </Card.Content>
       </Card>
     )
