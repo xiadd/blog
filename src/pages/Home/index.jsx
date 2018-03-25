c68b03e1f8e27c25d57d364d5efc3913dc4b7494
xiadd <xiadd0102@gmail.com>
2018-03-25 17:30:26 +0800

    change

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,32 @@
 import React, { Component } from 'react'
 import PostGroup from 'components/Posts'
 import { listIssues } from 'services/issue'
+import { Loader } from 'semantic-ui-react'
 
 class Home extends Component {
   constructor () {
     super()
     this.state = {
       posts: [],
-      current: 1
+      current: 1,
+      loading: true
     }
   }
 
   async componentDidMount () {
     const issues = await listIssues()
     this.setState({
-      posts: issues.data
+      posts: issues.data,
+      loading: false
     })
   }
   render () {
-    return <PostGroup dataset={this.state.posts} />
+    return (
+      <div>
+        <Loader active={this.state.loading} inline='centered' />
+        <PostGroup dataset={this.state.posts} />
+      </div>
+    )
   }
 }
 
